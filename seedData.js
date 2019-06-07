0bae457c68e2a237426b8c09201efa28a4ddba57
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-06-07 15:53:31 -0700

    Server has all the routes needed

---
+++ seedData.js
@@ -0,0 +1,24 @@
+const Question = require('./lib/models/questions');
+const Answer = require('./lib/models/answers');
+const chance = require('chance').Chance();
+
+module.exports = () => {
+  return Promise.all([...Array(60)].map(() => {
+    return Question
+      .create({
+        question: chance.sentence()
+      });
+  }))
+    .then(questions => {
+      console.log(questions);
+      return Promise.all([...Array(200)].map(() => {
+        return Answer
+          .create({
+            answer: chance.sentence(),
+            questionId: chance.pickone(questions)._id
+          });
+      }));
+    });
+};
+
+
